35d9dfaaa5583d00c396d0dd7ed54f1901a4ec97
ThanhChinhBK <nguyenthanhchinh96@gmail.com>
2017-11-21 00:05:05 +0700

    update data

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 python gym_news/pipelines/pre_crawl.py
-scrapy runspider gym_news/spiders/bgym.py -o gym_news/raw_data/data.jl -t jsonlines --set HTTPCACHE_ENABLED=0
+scrapy runspider gym_news/spiders/gym.py -o gym_news/raw_data/data.jl -t jsonlines --set HTTPCACHE_ENABLED=0
 python gym_news/pipelines/process_data.py
 git add -A
 git commit -m "update data"
